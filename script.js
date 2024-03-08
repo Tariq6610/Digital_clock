fb3e606ab946b3b18a2a422309fea11e9adc9ddd
Tariq6610 <115495245+Tariq6610@users.noreply.github.com>
2024-03-08 15:30:05 +0500

    24 hours clock updated to 12 hours clock

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let hours = document.querySelector("#hour")
 let minits = document.querySelector("#minit")
 let seconds = document.querySelector("#second")
 let day = document.querySelector("#day")
+let AM = document.querySelector("#AM")
 
 
 setInterval(() => {
@@ -9,11 +10,17 @@ setInterval(() => {
     let min = new Date().getMinutes();
     let sec = new Date().getSeconds();
     let currentDay = new Date().getDay()
+    if(hrs >= 12){
+        AM.innerText = "PM"
+    }
+    if(hrs > 12){
+        hrs -= 12
+    }
     hours.innerHTML = hrs;
     minits.innerHTML = min;
     seconds.innerHTML = sec;
     let weekDays = ["Sun","Mon","Tue","Wed","thu","Fri","Sat"]
     day.innerHTML = weekDays[currentDay];
-}, 10);
+}, 1000);
 
 
